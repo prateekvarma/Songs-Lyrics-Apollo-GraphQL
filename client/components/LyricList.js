4996e0c24bc10b7c8ecf7d490b2b7b0aef580b9e
Prateek Varma <prtk.varma@gmail.com>
2022-08-18 20:53:17 -0600

    Created Like button, and wired the GraphQL mutation with the click event

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 class LyricList extends Component {
+  onLike(id) {
+    this.props.mutate({
+      variables: { id: id },
+    });
+  }
+
   renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => {
       return (
         <li className="collection-item" key={id}>
           {content}
+          <i className="material-icons" onClick={() => this.onLike(id)}>
+            thumb_up
+          </i>
         </li>
       );
     });
@@ -16,4 +27,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
