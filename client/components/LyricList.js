9f0bf2e7a0ab5a5af5fbab471a3e47b08f046598
Prateek Varma <prtk.varma@gmail.com>
2022-08-19 20:36:54 -0600

    Modified GraphQL query to fetch likes per lyrics too, and displayed them

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -10,13 +10,16 @@ class LyricList extends Component {
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li className="collection-item" key={id}>
           {content}
-          <i className="material-icons" onClick={() => this.onLike(id)}>
-            thumb_up
-          </i>
+          <div className="vote-box">
+            <i className="material-icons" onClick={() => this.onLike(id)}>
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
